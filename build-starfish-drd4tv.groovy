8617c6a982a235fe21cba441f47e26343c0fbdb4
Sunjoo Park <allessunjoo.park@lge.com>
2017-01-10 16:59:27 +0900

    Second

--- build-starfish-drd4tv.groovy
+++ build-starfish-drd4tv.groovy
@@ -4,8 +4,14 @@ node('master'){
     DATA_ROOT = OPENGROK_INSTANCE_HOME + "/data"
     PROJECT_NAME = JOB_NAME.split('/')[1]
     PROJECT_DIRECTORY =  SRC_ROOT + "/" + PROJECT_NAME
-    echo PROJECT_DIRECTORY
+    echo "INFO: Project directory = " + PROJECT_DIRECTORY
     //sh 'cd ~/temp && git clone -b @drd4tv ssh://wall.lge.com/starfish/build-starfish'
+    project_dir = new File(PROJECT_DIRECTORY)
+    if ( project_dir.exits() ) {
+        echo "INFO: Exist = " + PROJECT_DIRECTORY
+    } else {
+        echo "INFO: Dont' exist = " + PROJECT_DIRECTORY
+    }
     stage 'Run mcf'
     echo JOB_NAME.split('/')[1]
     //sh 'cd ~/temp/build-starfish && ./mcf -b 0 -p 0 m16p --premirror=file:///starfish/starfish/dreadlocks/downloads/'
