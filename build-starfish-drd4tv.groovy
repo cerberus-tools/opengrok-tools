995fd3cfc559ef88500753d27fad072be8fd95da
Sunjoo Park <allessunjoo.park@lge.com>
2017-01-10 17:14:51 +0900

    Update

--- build-starfish-drd4tv.groovy
+++ build-starfish-drd4tv.groovy
@@ -6,15 +6,19 @@ node('master'){
     PROJECT_DIRECTORY =  SRC_ROOT + "/" + PROJECT_NAME
     echo "INFO: Project directory = " + PROJECT_DIRECTORY
     //sh 'cd ~/temp && git clone -b @drd4tv ssh://wall.lge.com/starfish/build-starfish'
-    if ( fileExists(PROJECT_DIRECTORY) ) {
-        echo "INFO: Exist = " + PROJECT_DIRECTORY
-    } else {
-        echo "INFO: Dont' exist = " + PROJECT_DIRECTORY
-    }
     stage 'Run mcf'
-    echo JOB_NAME.split('/')[1]
-    //sh 'cd ~/temp/build-starfish && ./mcf -b 0 -p 0 m16p --premirror=file:///starfish/starfish/dreadlocks/downloads/'
+    
+    def mcf_command = ""
+    if ( fileExists(PROJECT_DIRECTORY + "/mcf.status" ) ) {
+        echo "INFO: Exist = " + PROJECT_DIRECTORY
+        mcf_command = "./mcf --command update"
+    } else {
+        sh "rm -rf " + PROJECT_DIRECTORY        
+        sh "git clone -b @drd4tv ssh://wall.lge.com/starfish/build-starfish " + PROJECT_DIRECTORY
+        mcf_command = "./mcf -b 0 -p 0 m16p --premirror=file:///starfish/starfish/dreadlocks/downloads"
+    }
+    sh "cd " + PROJECT_DIRECTORY + " && " + mcf_command
     stage 'Run build'
     echo JOB_NAME.split('/')[1]
-    //sh 'cd ~/temp/build-starfish && . ./oe-init-build-env && bitbake -c patchall starfish-atsc-flash '
+    echo "END"
 }
