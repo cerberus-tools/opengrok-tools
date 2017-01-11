e3ff07423e66fa225cdd5f4329485b5d9ac3c584
Sunjoo Park <allessunjoo.park@lge.com>
2017-01-11 10:34:24 +0900

    Remove unused codes

--- build-starfish-drd4tv.groovy
+++ build-starfish-drd4tv.groovy
@@ -1,5 +1,4 @@
 node('master'){
-    //PROJECT_NAME = "starfish-drd4tv-m16p"
     BUILD_IMAGES = [
         "m16p": "starfish-atsc-flash starfish-arib-flash starfish-dvb-flash",
         "m16pbno": "starfish-dvb-flash",
@@ -10,11 +9,9 @@ node('master'){
         "k3lp": "starfish-arib-flash starfish-dvb-flash"
     ]
 
-    stage 'get codes'
+    stage 'Set env '
     SRC_ROOT = OPENGROK_INSTANCE_HOME + "/src"
     DATA_ROOT = OPENGROK_INSTANCE_HOME + "/data"
-    //PROJECT_NAME = JOB_NAME.split('/')[1]
-    //PROJECT_NAME = "starfish-drd4tv-m16p"
     PROJECT_DIRECTORY =  SRC_ROOT + "/" + PROJECT_NAME
     BUILD_BRANCH = PROJECT_NAME.split("-")[1]
     BUILD_MACHINE = PROJECT_NAME.split("-")[2]
@@ -22,7 +19,7 @@ node('master'){
     BUILD_COMMAND = " . ./oe-init-build-env && bitbake -c patchall " + BUILD_IMAGES[BUILD_MACHINE]
     echo "INFO: Build command = " + BUILD_COMMAND
 
-    stage 'Run mcf'
+    stage 'Run mcf & build'
     def mcf_command = ""
     if ( fileExists(PROJECT_DIRECTORY + "/mcf.status" ) ) {
         echo "INFO: Exist = " + PROJECT_DIRECTORY
@@ -32,9 +29,4 @@ node('master'){
         sh "git clone -b @drd4tv ssh://wall.lge.com/starfish/build-starfish " + PROJECT_DIRECTORY
         mcf_command = "./mcf -b 0 -p 0 " +  BUILD_MACHINE + " --premirror=file:///starfish/starfish/dreadlocks/downloads"
     }
-
-    stage 'Run build'
-    sh " cd " + PROJECT_DIRECTORY + " && " +  mcf_command + " && " + BUILD_COMMAND
-
-    echo "END"
 }
