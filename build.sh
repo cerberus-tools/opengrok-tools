13ad060324c4fa07aff5d48dad0c504ac74e88db
Sunjoo Park <allessunjoo.park@lge.com>
2017-01-13 09:04:28 +0900

    Add 'BUILD_PLATFORM'

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ BUILD_BRANCH=${job_name[1]}
 BUILD_MACHINE=${job_name[2]}
 BUILD_IMAGES="starfish-atsc-flash"
 BUILD_REPO_URL="ssh://gpro.lgsvl.com/starfish/build-starfish"
+BUILD_PLATFORM="master"
 
 # Set BUILD_IMAGES
 case ${BUILD_MACHINE} in
@@ -33,10 +34,24 @@ DATA_ROOT="${OPENGROK_INSTANCE_HOME}/data"
 PROJECT_DIRECTORY="${SRC_ROOT}/${BUILD_NAME}"
 #BUILD_COMMAND=". ./oe-init-build-env & bitbake -c patchall ${BUILD_IMAGES}"
 BUILD_COMMAND="bitbake -c patchall ${BUILD_IMAGES}"
+
+# Check Build Platform
+case ${BUILD_BRANCH} in 
+    master)
+        BUILD_PLATFORM="master"
+        ;;
+    drd4tv|*dixie*)    
+        BUILD_PLATFORM="dreadlocks"
+        ;;
+esac
+
+
+# Check Build Branch and Build Repository Url
 if [ ! "${BUILD_BRANCH}" = "master" ]; then
     BUILD_BRANCH="@${BUILD_BRANCH}"
     BUILD_REPO_URL="ssh://wall.lge.com/starfish/build-starfish"
 fi
+
 MCF_COMMAND=""
 echo ${BUILD_BRANCH}
 echo ${BUILD_REPO_URL}
@@ -46,7 +61,7 @@ if [ -f "${PROJECT_DIRECTORY}/mcf.status" ]; then
     MCF_COMMAND="./mcf --command update"
 else
     echo "INFO: New Build Triggered"
-    MCF_COMMAND="./mcf -b 44 -p 44 ${BUILD_MACHINE} --premirror=file:///starfish/starfish/dreadlocks/downloads"
+    MCF_COMMAND="./mcf -b 44 -p 44 ${BUILD_MACHINE} --premirror=file:///starfish/starfish/${BUILD_PLATFORM}/downloads"
     rm -rf ${PROJECT_DIRECTORY}
     git clone -b "${BUILD_BRANCH}" ${BUILD_REPO_URL} ${PROJECT_DIRECTORY}
 fi
